f1306ad27943c9fe600a050434b3f8a288cfff3f
willy4opea <willy4opera@gmail.com>
2023-03-27 02:15:00 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -9,8 +9,8 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int i, printed_chars = 0;
+	int buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -32,16 +32,7 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
 		}
 	}
 
