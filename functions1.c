e885c03c8c8367cba45f002e7754bd959da15671
willy4opea <willy4opera@gmail.com>
2023-03-27 16:30:53 -0700

    Handle the following conversion specifiers: u, o, x, X

--- functions1.c
+++ functions1.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/************************* PRINT UNSIGNED NUMBER *************************/
+/************************* THIS FUNCTION PRINTS UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
