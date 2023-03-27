d9db424590f146e4ee6c1fb937de2f86c01a2ef3
willy4opea <willy4opera@gmail.com>
2023-03-27 16:57:34 -0700

    Handle the following flag characters for non-custom conversion specifiers:.

--- get_precision.c
+++ get_precision.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - This function Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
+ * @format: Formatted string in which t will print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
