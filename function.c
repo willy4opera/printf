17555fd7931550fef0872c66429e3db1a0d4c9f2
Aminu Dzara <aminudzara@gmail.com>
2023-03-28 12:50:08 +0000

    Update function that handles printing a character

--- function.c
+++ function.c
@@ -15,9 +15,9 @@
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	char c = va_arg(types, int);
+	char ch = va_arg(types, int);
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+	return (handle_write_char(ch, buffer, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
 /**
