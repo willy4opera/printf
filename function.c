c88665b10e30e1dd617ee505938d6e5f2b0563d9
Aminu Dzara <aminudzara@gmail.com>
2023-03-28 12:57:06 +0000

    Update

--- function.c
+++ function.c
@@ -175,7 +175,7 @@ int print_binary(va_list types, char buffer[],
 	}
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
-		sum += a[i];
+		sum = sum + a[i];
 		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
