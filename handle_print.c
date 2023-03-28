742a6e8b0c691d5e04ac4e2b529466f417792b5e
Aminu Dzara <aminudzara@gmail.com>
2023-03-28 08:58:26 +0000

    Update logic

--- handle_print.c
+++ handle_print.c
@@ -31,7 +31,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknown_leng += write(1, "%%", 1);
+		unknown_leng = unknown_leng + write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknown_leng = unknown_leng + write(1, " ", 1);
 		else if (width)
