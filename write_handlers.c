b3ac3bca459993f1e0f9caf1fb1318993572b91b
Aminu Dzara <aminudzara@gmail.com>
2023-03-28 13:20:08 +0000

    Update

--- write_handlers.c
+++ write_handlers.c
@@ -163,7 +163,7 @@ int write_unsgnd(int is_negative, int ind,
 	if ((precision == 0) && (ind == BUFF_SIZE - 2) && (buffer[ind] == '0'))
 		return (0); /* printf(".0d", 0)  no char is printed */
 
-	if (precision > 0 && precision < length)
+	if ((precision > 0) && (precision < length))
 		padd = ' ';
 
 	while (precision > length)
