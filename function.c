0b713c6eeb2117e5d47fb8a35d94f2d4d3734883
Aminu Dzara <aminudzara@gmail.com>
2023-03-28 12:55:31 +0000

    Update function that handles printing binary

--- function.c
+++ function.c
@@ -170,7 +170,7 @@ int print_binary(va_list types, char buffer[],
 	a[0] = n / m;
 	for (i = 1; i < 32; i++)
 	{
-		m /= 2;
+		m = m / 2;
 		a[i] = (n / m) % 2;
 	}
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
